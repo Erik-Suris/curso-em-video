dae79af4b02bb18102e0c769d7fba52fc22d2024
Erik <erikvieira9016@gmail.com>
2022-11-07 16:03:25 -0300

    Commit

--- JavaScript/Aula13/ambiente.js
+++ JavaScript/Aula13/ambiente.js
@@ -7,9 +7,35 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
 
-var count = 0
+// While
+/*
+var count = 1
 
-while (count < 1) {
-    console.log('Tudo bem?')
+while (count <= 10) {
+    console.log(`Passo ${count}`)
     count++
 }
+*/
+
+// Do while
+/*
+var count = 1
+
+do {
+    console.log(`Passo ${count}`)
+    count++
+} while (count <= 10)
+*/
+
+// For
+/*
+for (var count = 1; count <= 10; count++) {
+    console.log(`Passo ${count}`)
+}
+*/
+
+console.log('Vai começar...')
+for (var count = 1; count <= 10; count++) {
+    console.log(`Passo ${count}`)
+}
+console.log('FIM!')
