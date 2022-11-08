b8cf0f6e70a03d887a1565d7035ddc76dcb608b1
Erik <erikvieira9016@gmail.com>
2022-11-08 12:58:36 -0300

    Commit

--- JavaScript/Aula14ex/ex017/script.js
+++ JavaScript/Aula14ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function gerar() {
+    let num = document.getElementById('inum')
+    let tab = document.getElementById('itab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = 'tab${c}'
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
